e0ec2d2ee1cdd6e1deb8b9188f4f822f08e653bd
Jonathan Norman <jnorman2123@gmail.com>
2019-10-17 21:02:17 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -12,7 +12,7 @@ class Anagram
     match_words.collect do |match_word|
       match_word = match_word.split("").sort!
       match_word.each do |split_word|
-        if split_word == word_array
+        if (split_word - word_array).empty == true
           return split_word
         else
           []
