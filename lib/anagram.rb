2c744c2689d20c046f9c0054ab393726cfa99a39
Jonathan Norman <jnorman2123@gmail.com>
2019-10-17 20:59:08 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -11,13 +11,13 @@ class Anagram
     word_array = @word.split("").sort!
     match_words.collect do |match_word|
       match_word = match_word.split("").sort!
-      # match_word.each do |split_word|
-      #   if split_word == word_array
-      #     return split_word
-      #   else
-      #     []
-      #   end
-      binding.pry
+      match_word.each do |split_word|
+        if split_word == word_array
+          return split_word
+        else
+          []
+        end
+      end
     end
   end
 end
