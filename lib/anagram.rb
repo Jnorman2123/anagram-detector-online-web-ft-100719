746f1d2307d6caffe65ec83daf324f353ab511d7
Jonathan Norman <jnorman2123@gmail.com>
2019-10-17 21:21:25 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,7 +8,7 @@ class Anagram
   end
 
   def match(match_words)
-    word_array = @word.split("").sort!
+    word_array = @word.split.sort!
     match_words.collect do |match_word|
       match_word = match_word.split.sort!
       if match_word == word_array
